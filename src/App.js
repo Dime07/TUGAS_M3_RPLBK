753f341ecd4157b1b81405a7efc12c2c9041dad0
Dime07 <dimasbkt7@gmail.com>
2022-10-25 14:30:43 +0700

    logic program

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Main from "./pages/main";
 
 function App() {
+
+  const [chat, setChat] = useState([]);
+  const [leftChat, setLeftChat] = useState({});
+  const [rightChat, setRightChat] = useState({});
+
+
+  function sendToLeft(){
+    setChat([...chat, leftChat]);
+    document.getElementById('input-left').value = ''
+  }
+
+  function sendToRight(){
+    setChat([...chat, rightChat]);
+    document.getElementById('input-right').value = ''
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Fake Chat Flter App
+      </p>
+      <p>
+        This word not allowed (shit)
+      </p>
+      <Main chat={chat}/>
+      <div>
+        <div>
+          <input id="input-left" onChange={(e) => setLeftChat({message: e.target.value, type: 'left'})}/>
+          <button onClick={sendToLeft}>Kirim </button>
+        </div>
+
+        <div>
+          <input id="input-right" onChange={(e) => setRightChat({message: e.target.value, type: 'right'})}/>
+          <button  onClick={sendToRight}>Kirim </button>
+        </div>
+
+      </div>
     </div>
   );
 }
