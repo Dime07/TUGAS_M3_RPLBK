395580981664e1c1fa6a9ba1a01d63a9a9d74941
Chairullatif Aji Sadewa <khoirullatif337@gmail.com>
2022-10-27 22:56:02 +0700

    Change Background Color

--- src/components/leftChat.js
+++ src/components/leftChat.js
@@ -1,27 +1,21 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react';
 
-export default function LeftChat(props){
-    const bubble = {
-        padding: '10px',
-        borderRadius: '3px 5px 3px 5px',
-        backgroundColor: 'purple',
-        width: 'fit-content',
-        height: 'fit-content',
+export default function LeftChat(props) {
+  const bubble = {
+    padding: '10px',
+    borderRadius: '3px 5px 3px 5px',
+    backgroundColor: 'yellow',
+    width: 'fit-content',
+    height: 'fit-content',
+  };
+
+  const [message, setMessage] = useState(props.message);
+
+  useEffect(() => {
+    if (props.message.includes('shit')) {
+      alert('heei anda berkata kasar');
     }
+  }, [message]);
 
-    const [message, setMessage] = useState(props.message)
-
-    useEffect(() => {
-      if(props.message.includes('shit')){
-        alert ('heei anda berkata kasar')
-      }
-        
-    },[message])
-    
-
-    return (
-        <div style={bubble}>
-            {message}
-        </div>
-    )
+  return <div style={bubble}>{message}</div>;
 }
